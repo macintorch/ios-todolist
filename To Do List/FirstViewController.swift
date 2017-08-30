1a7e1bc386bb3ec14b2e34a56975832c4bbebeb3
Ainor Syahrizal <macintorch@gmail.com>
2017-08-31 03:15:56 +0800

    Initial Commit

---
+++ To Do List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Ainor Syahrizal on 31/08/2017.
+//  Copyright © 2017 Ainor Syahrizal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
