317802592eca5d184c4e371741ec2a39f99cdc63
Ainor Syahrizal <macintorch@gmail.com>
2017-08-31 03:40:01 +0800

    first VC table view setup

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return 1
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create cell
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
